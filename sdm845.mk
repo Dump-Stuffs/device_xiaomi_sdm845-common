0490f8aee662fd8aaa84a6d30295da344891d171
AdarshGrewal <adarshgrewal@gmail.com>
2021-02-10 08:12:43 +0530

    sdm825-common: overlay: adjust qqs for notch
    *remove and cleanup soon!

--- sdm845.mk
+++ sdm845.mk
@@ -21,7 +21,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-dot
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep \
