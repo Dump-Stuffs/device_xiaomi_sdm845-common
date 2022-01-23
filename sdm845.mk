739c5b95a67613265d518d5698c12566916feec6
Cruzer1 <preetamswain143@gmail.com>
2022-01-23 04:11:44 +0000

    sdm845-common:disable injecting some bootjars

--- sdm845.mk
+++ sdm845.mk
@@ -190,8 +190,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+#PRODUCT_BOOT_JARS += \
+#    org.ifaa.android.manager
 
 # Input
 PRODUCT_COPY_FILES += \
@@ -357,5 +357,5 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
